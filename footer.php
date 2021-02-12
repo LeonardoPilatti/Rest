f1d85efa4c0ede363155e553136d68c64822cab7
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-02-12 19:30:31 -0300

    começando o projeto de Wordpress, passando os arquivos para PHP

---
+++ footer.php
@@ -0,0 +1,18 @@
+		<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="/testewp.test/">Menu</a></li>
+					<li><a href="http://testewp.test/sobre/">Sobre</a></li>
+					<li><a href="/testewp.test/contato/">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2021. Alguns direitos reservados.</p>
+			<p>Desenvolvido por Leonardo Pilatti.</p>
+		</footer>
+
+		<!-- Footer Wordpress -->
+		<?php wp_footer(); ?>
+		<!-- Fecha Footer Wordpress -->
+	</body>
+</html>
