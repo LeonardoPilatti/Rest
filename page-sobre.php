7a4672975552cfa2d5447557174d2bc2ab7d75a1
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-02-15 10:15:41 -0300

    projeto totalmente responsivo pelo wordpress

--- page-sobre.php
+++ page-sobre.php
@@ -11,13 +11,12 @@
 			</div>
 
 			<div class="grid-8">
-				<h2>História</h2>
-				<p>O incentivo ao avanço tecnológico, assim como a mobilidade dos capitais internacionais auxilia a preparação e a composição dos níveis de motivação departamental.</p>
-				<p>Gostaria de enfatizar que o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes do remanejamento dos quadros funcionais.</p>
-				<h2>Visão</h2>
-				<p>Não obstante, a expansão dos mercados mundiais faz parte de um processo de gerenciamento de alternativas às soluções ortodoxas.</p>
-				<h2>Valores</h2>
-				<p>O empenho em analisar a consolidação das estruturas apresenta tendências no sentido de aprovar a manutenção dos índices pretendidos.</p>
+				<h2><?php the_field('historia'); ?></h2>
+				<p><?php the_field('textoHistoria'); ?></p>
+				<h2><?php the_field('visao'); ?></h2>
+				<p><?php the_field('textoVisao'); ?></p>
+				<h2><?php the_field('valores'); ?></h2>
+				<p><?php the_field('textoValores'); ?></p>
 			</div>
 		</section>
 
